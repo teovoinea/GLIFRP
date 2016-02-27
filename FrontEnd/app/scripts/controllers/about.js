765d93dcb98b6e6c39944398f08ea25f00f320eb
flickerinhouse <rtemelkovski@yahoo.com>
2016-02-27 13:55:35 -0500

    You can run front end server now

---
+++ FrontEnd/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name frontEndApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the frontEndApp
+ */
+angular.module('frontEndApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
