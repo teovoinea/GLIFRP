2b0b9d0c47de908da95fd1a289eb5ca9f8988c06
Teo <voinea.teodor@gmail.com>
2016-02-28 14:13:26 -0500

    Added search engpoint with a mirroring response on POST

--- BackEnd/src/main/java/Main.java
+++ BackEnd/src/main/java/Main.java
@@ -7,5 +7,27 @@ public class Main {
         get("/test", (req, res) -> 
         	"<img src='/img/2.PNG'/>"
         );
+
+        post("/search", (request, response) ->{
+        	try{
+	        	String latitude = request.attribute("lat");
+	        	String longitude = request.attribute("lon");
+	        	String zip_code = request.attribute("zip");
+        		response.type("application/javascript");
+        		response.status(200);
+        		String r = String.format("{
+        						lat:%s,
+        						lon:%s,
+        						zip:%s
+        					}", latitude, longitude, zip_code);
+        		return r;
+        	}
+        	catch (Exception e){
+        		response.type("text/plain");
+        		response.status(500);
+        		e.printStackTrace();
+        		return e.toString();
+        	}
+        });
     }
 }
