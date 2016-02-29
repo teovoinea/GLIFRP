1088bd983d2862b5144c6cf7019c38f62a7631f6
Teo <voinea.teodor@gmail.com>
2016-02-29 14:02:35 -0500

    Created Sorting algorithm interface that will use different sorting algorithms based on the attribute you are sorting by

---
+++ BackEnd/src/main/java/Searching.java
@@ -0,0 +1,48 @@
+public interface Searching{
+
+	/**
+	 * Sorts the comparable array by any of the following attributes
+	 * 0.
+	 * 1.
+	 * 2.
+	 * 3.
+	 * .
+	 * .
+	 * .
+	 * @param attribute [description]
+	 */
+	public static void SortByType(int attribute){
+		if (attribute == 0){
+
+		}
+		if (attribute == 1){
+
+		}
+		if (attribute == 2){
+
+		}
+		if (attribute == 3){
+
+		}
+	}
+
+	private static void mergeSort(){
+
+	}
+
+	private static void quickSort(){
+
+	}
+
+	private static void quickSort3way(){
+
+	}
+
+	private static void shellSort(){
+
+	}
+
+	private static void heapSort(){
+
+	}
+}
