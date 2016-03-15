67d8b900def722157f7f973be393492801715ea8
Nickolas A. Morrison <nickmorrison09@gmail.com>
2016-03-14 21:27:20 -0400

    Receive from city

--- BackEnd/src/main/resources/public/src/js/app.js
+++ BackEnd/src/main/resources/public/src/js/app.js
@@ -10,7 +10,40 @@
     //.when('/', {templateUrl: 'views/main.html', controller: 'mainController'})
     //.otherwise({redirectTo: '/'});
 
-    $locationProvider.html5Mode(true);
+    //$locationProvider.html5Mode(true);
   }]);
-  
+  app.run(function(){
+
+    function detectType(input){
+      if(input.match(/[a-z]/i)){
+        return "CITY_STATE";
+      }else{
+        return "LONG_LAT";
+      }
+    }
+
+    $("#search-box").keyup(function(event){
+      if(event.keyCode == 13){
+        var info = $("#search-box").val().split(",");
+        if(detectType($("#search-box").val()) == "CITY_STATE"){
+          $.ajax({
+            url: "/search",
+            type: "POST",
+            data: '{"city":"'+info[0]+'", "state":"'+info[1]+'"}'
+          }).done(function(res){
+            console.log(res);
+          });
+        }else{
+          $.ajax({
+            url: "/search",
+            type: "POST",
+            data: {"lat":info[0], "lon":info[1]}
+          }).done(function(res){
+            console.log(res);
+          });
+        }
+      }
+    });
+  });
+
 })();
