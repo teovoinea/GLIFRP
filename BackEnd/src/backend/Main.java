e6505ae2a01b25735b12421436f80441e98b4835
carlossantana3279 <carlossantana3279@gmail.com>
2016-04-02 10:50:31 -0400

    score

--- BackEnd/src/backend/Main.java
+++ BackEnd/src/backend/Main.java
@@ -106,7 +106,6 @@ public class Main {
                 	mapwrap.buildByCityState(c.getName(), c.getUState());
                     c.setLat(mapwrap.getLat());
                     c.setLong(mapwrap.getLon());
-                    c.setScore();
                     final_out.add(q.getCityByCrime(c));
                     
                 }
